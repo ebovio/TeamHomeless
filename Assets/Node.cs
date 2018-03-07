0799d6a8eb277fffc6bba9c923a05fc4a4903199
sebasexy <spider_cbaz_10@hotmail.com>
2018-03-06 18:03:48 -0600

    Initial stuff

---
+++ Assets/Node.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+
+    public Node[] neighbors;
+    public List<Node> history;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        for (int i = 0; i < neighbors.Length; i++)
+        {
+            Gizmos.DrawLine(transform.position, neighbors[i].transform.position);
+        }
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(transform.position, 0.5f);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, 0.5f);
+    }
+}
